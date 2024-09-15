969e675f44c6e557a0d0ad980175d5da26295a14
Damian Sire <damiansire@gmail.com>
2024-09-15 08:38:28 -0300

    feat: add getCountriesByContinent function

--- src/libs/continent.js
+++ src/libs/continent.js
@@ -1,9 +1,16 @@
 const countryCodes = require("./countryCodes");
 
-africa;
-asia;
-europe;
-northamerica;
-oceania;
-southamerica;
-antarctica;
+/*
+ * Allowed continent names:
+ * africa
+ * asia
+ * europe
+ * northamerica
+ * oceania
+ * southamerica
+ * antarctica
+ */
+function getCountriesByContinent(continent) {
+  const filteredCountries = codes.filter((country) => country.continent.toLowerCase() === continent.toLowerCase());
+  return filteredCountries;
+}
